d05f3cff8cab129af36c1cc4f7bfbfe9143e57f1
Isha <ishasingh@ibm.com>
2025-06-16 21:24:40 +0530

    lol

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    visualizer({
-      open: true, // Automatically opens report in browser
-      filename: 'dist/report.html',
-      gzipSize: true,
-      brotliSize: true,
-    })
-  ],
+  plugins: [react()],
 });
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { visualizer } from 'rollup-plugin-visualizer';
+
+// export default defineConfig({
+//   base: './',
+//   plugins: [
+//     react(),
+//     visualizer({
+//       open: true, // Automatically opens report in browser
+//       filename: 'dist/report.html',
+//       gzipSize: true,
+//       brotliSize: true,
+//     })
+//   ],
+// });
 
 
 
